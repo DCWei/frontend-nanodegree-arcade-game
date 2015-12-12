d2a71f00761ef576f18d2e63293c151b6a3253af
Wayne Hsieh <john00184@gmail.com>
2015-12-12 14:29:34 +0800

    Finish OO Javascript final project

--- js/app.js
+++ js/app.js
@@ -1,11 +1,20 @@
+var totalWidth = 5*101;
+var totalHeight = 6*83;
+var blockWidth = 101;
+var blockHeight = 83;
+var WidthblockMaxNum = 5;
+var HeightblockMaxNum = 6;
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(AxisX, AxisY) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = AxisX*101;
+    this.y = AxisY*83;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +23,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if((this.x + dt*blockWidth) < 0)
+        this.x = (this.x + dt*blockWidth)%totalWidth+totalWidth;
+    else
+        this.x = (this.x + dt*blockWidth)%totalWidth;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +37,51 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+var Player = function(character) {
+    this.sprite = "images/char-"+character+".png";
+    console.log(this.sprite);
+    this.x = 2*blockWidth;
+    this.y = 5*blockHeight;
+};
+Player.prototype.update = function() {
+    if(this.y <= 0*blockHeight)
+    {
+        console.log('player wins!! Start a new game.');
+        this.x = 2*blockWidth;
+        this.y = 5*blockHeight;
+        return;
+    }
+    for(i=0;i<allEnemies.length;i++)
+    {
+        //console.log('allEnemies['+ i +'].x='+allEnemies[i].x+', allEnemies['+ i +'].y='+allEnemies[i].y);
+        if(this.y == allEnemies[i].y && 
+           ((this.x > allEnemies[i].x && this.x < allEnemies[i].x + blockWidth) || (this.x + blockWidth > allEnemies[i].x && this.x + blockWidth < allEnemies[i].x + blockWidth)))
+        {   
+            console.log('player dies!! Restart the game.');
+            this.x = 2*blockWidth;
+            this.y = 5*blockHeight;
+            break;
+        }
+    }
+};
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+Player.prototype.handleInput = function(key) {
+    if(key === 'left' && this.x > 0*blockWidth)
+        this.x = this.x - blockWidth;
+    else if(key === 'right' && this.x < (WidthblockMaxNum-1)*blockWidth)
+        this.x = this.x + blockWidth;
+    else if(key === 'up' && this.y > 0*blockHeight)
+        this.y = this.y - blockHeight;
+    else if(key === 'down' && this.y < (HeightblockMaxNum-1)*blockHeight)
+        this.y = this.y + blockHeight;
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy(0,1), new Enemy(0,2)];
+var player = new Player("cat-girl");
 
 
 // This listens for key presses and sends the keys to your
